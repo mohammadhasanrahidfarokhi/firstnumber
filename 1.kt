bcb48481cfa27d76e759193d228d0d9cf0ed36b7
mohammad <mmd1380lol@gmail.com>
2023-05-29 22:13:36 +0330

    rashidfarokhi

--- 1.kt
+++ 1.kt
@@ -1,7 +1,7 @@
 fun main() {
-//    val start = 100
-//    val end = 999
-    for (i in 2..50) {
+    val start = 100
+    val end = 999
+    for (i in start..end) {
         var l = true
         for (j in 2 until i) {
             if (i % j == 0) {
