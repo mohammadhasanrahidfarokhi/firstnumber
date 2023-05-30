3b504b9ee36957e0cf9c24ef31ab545344c0ecee
mohammadhasanrahidfarokhi <132354127+mohammadhasanrahidfarokhi@users.noreply.github.com>
2023-05-30 14:32:50 +0330

    1.kt

--- 1.kt
+++ 1.kt
@@ -3,14 +3,14 @@ fun main() {
     val end = 999
     for (i in start..end) {
         print("$i,")
-//        var l = true
-//        for (j in 2 until i) {
-//            if (i % j == 0) {
-//                l = false
-//                break
-//            }
-//        }
-//        if (l)
-//            print("$i ")
+       var l = true
+       for (j in 2 until i) {
+          if (i % j == 0) {
+                l = false
+               break
+            }
+        }
+        if (l)
+           print("$i ")
     }
 }
